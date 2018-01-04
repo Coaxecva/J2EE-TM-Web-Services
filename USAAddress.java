0f6d5e886732768f2c52188d42a65841d2ff1a8f
Quang Tran <tmquang@gmail.com>
2018-01-03 23:39:10 -0600

    Create USAAddress.java

---
+++ USAAddress.java
@@ -0,0 +1,19 @@
+/**
+<complexType name="USAddress">
+<sequence>
+<element name="name" type="string" />
+<element name="street" type="string" />
+<element name="city" type="string" />
+<element name="state" type="string" />
+<element name="zip" type="string" />
+</sequence>
+</complexType>
+*/
+
+public class USAddress {
+  public String name;
+  public String street;
+  public String city;
+  public String state;
+  public String zip;
+}
