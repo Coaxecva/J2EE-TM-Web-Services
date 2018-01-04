e6137e97a257412c982ca0e27be53a1cac1e62bb
Quang Tran <tmquang@gmail.com>
2018-01-03 23:34:19 -0600

    Update purchaseOrder.java

--- purchaseOrder.java
+++ purchaseOrder.java
@@ -1,7 +1,8 @@
 package com.monsonhaefel.jwsbook;
+
 public class PurchaseOrder {
-String accountName;
-int accountNumber;
-float total;
-// more stuff follows
+  String accountName;
+  int accountNumber;
+  float total;
+  // more stuff follows
 }
