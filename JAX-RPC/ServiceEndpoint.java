59cb0a4f8bad90367bc173de80fd40a4a2c12935
Quang Tran <tmquang@gmail.com>
2018-01-21 23:34:39 -0600

    Update ServiceEndpoint.java

--- JAX-RPC/ServiceEndpoint.java
+++ JAX-RPC/ServiceEndpoint.java
@@ -9,3 +9,29 @@ public class BookQuote_Impl_1 implements BookQuote {
     return 24.99f;
   }
 }
+
+// An EJB Endpoint's Stateless Session Bean
+public class BookPriceWS implements javax.ejb.SessionBean, BookQuote {
+  public void setSessionContext(javax.ejb.SessionContext ctx){}
+  public void ejbCreate(){}
+  public float getBookPrice(String isbn){
+    return 24.99f;
+  }
+  public void ejbRemove(){}
+  public void ejbActivate(){}
+  public void ejbPassivate(){}
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//a J2EEcomponent uses JAX-RPC DII to get the wholesale price of a book from someWeb service.
+/////////////////////////////////////////////////////////////////////////////////////////////
+InitialContext jndiContext = new InitialContext ( );
+javax.xml.rpc.Service service = (javax.xml.rpc.Service)
+                                jndiContext.lookup("java:comp/env/service/DynamicService")
+QName port = new QName("http://www.xyz.com/BookQuote ","BookQuote");
+QName operation = new QName("http://www.xyz.com/BookQuote","getBookPrice");
+Call callObject = service.createCall(port, operation);
+Object [] parameters = new Object[1];
+parameters[0] = isbn;
+Float price = (Float) callObject.invoke( parameters );
